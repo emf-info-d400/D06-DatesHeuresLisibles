f1ef77e0ca35475b3f324d0dec58e9ec05ff6cf0
Paul Friedli <paul.friedli@edufr.ch>
2023-06-05 19:48:38 +0200

    Consigne initiale

---
+++ src/services/MesFormateurs.java
@@ -0,0 +1,17 @@
+package services;
+
+public class MesFormateurs {
+
+        public static String dateActuelleJJMMAAAA() {
+                return "";
+        }
+
+        public static String heureActuelleHHMMSS() {
+                return "";
+        }
+
+        public static String dateEtHeureActuelleABerne() {
+                return "";
+        }
+
+}
